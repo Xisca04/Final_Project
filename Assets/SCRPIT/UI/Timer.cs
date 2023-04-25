c9f0e86828d26b151a3eb590b0e4675d6fd085ff
Xisca04 <114388909+Xisca04@users.noreply.github.com>
2023-04-25 19:21:12 +0200

    timer

---
+++ Assets/SCRPIT/UI/Timer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    
+    public float timer = 0;
+    public TextMeshProUGUI textTime;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        textTime.text = "" + timer.ToString("f1");
+    }
+
+}
