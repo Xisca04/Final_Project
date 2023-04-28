3e3a2cc12272505747464dcd554f5ed6815199bf
Xisca04 <xiscacerdavicens@gmail.com>
2023-04-28 13:03:34 +0200

    timer_stop

--- Assets/SCRPIT/UI/Timer.cs
+++ Assets/SCRPIT/UI/Timer.cs
@@ -7,13 +7,14 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     
-    public float timer = 0;
+    public float timer;
     public TextMeshProUGUI textTime;
 
     private void Update()
     {
         timer -= Time.deltaTime;
         textTime.text = "" + timer.ToString("f1");
+
     }
 
 }
