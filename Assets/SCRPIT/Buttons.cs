7f4b6209d8174f54efa326f298baebc47e8cae98
Xisca04 <xiscacerdavicens@gmail.com>
2023-05-08 09:26:12 +0200

    buttons

--- Assets/SCRPIT/Buttons.cs
+++ Assets/SCRPIT/Buttons.cs
@@ -5,8 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
+    private void Start()
+    {
+        instructionsPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+    }
+    
+    // Cambio escena al 1r nivel
     public void GoToScene(int sceneIDX)
     {
         SceneManager.LoadScene(sceneIDX);
     }
+
+    public GameObject instructionsPanel;
+
+    public void InstructionsOn()
+    {
+        instructionsPanel.SetActive(true);
+    }
+
+    public void ReturnMain()
+    {
+        instructionsPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+    }
+
+    public GameObject optionsPanel;
+    public void OptionsOn()
+    {
+        optionsPanel.SetActive(true);
+    }
+
+    
 }
